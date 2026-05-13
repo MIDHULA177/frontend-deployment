699069eb92e7a8002472831177c0607cbce2e0ad
MIDHULA177 <midhula.v2024aids@sece.ac.in>
2026-05-13 12:40:37 +0530

    Update App.jsx

--- react-profile-main/src/App.jsx
+++ react-profile-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>AWS</h1>
+          <h1>Midhula V 2024</h1>
           <p>
           <code>CI CD</code>
           </p>
